3cc4382dc8f08039dd324a8e842b3dd74565ab27
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2022-08-23 19:55:57 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 #!/bin/python3
 import threading, socket, os, sys
-
 class bcolors:
     CYAN = '\033[96m'
     GREEN = '\033[92m'
     YELLOW = '\033[93m'
     RED = '\033[91m'
     ENDC = '\033[0m'
-    
 def error(host,port,arg):
 	print(f"{bcolors.RED}[!]{bcolors.YELLOW} {host}{bcolors.GREEN}:{bcolors.CYAN}{port} {bcolors.ENDC}| {bcolors.RED}{arg}{bcolors.ENDC}")
-
 def no_error(host,port,arg):
 	print(f"{bcolors.YELLOW}{host}{bcolors.GREEN}:{bcolors.CYAN}{port} {bcolors.ENDC}| {bcolors.GREEN}{arg}{bcolors.ENDC}")
-
 def execute_ssh(host,port,username,password,command):
    password=password.replace("\n","")
    check=os.popen(f"sshpass -p {password} ssh -o StrictHostKeyChecking=no {username}@{host} -p {port} whoami 2> /dev/null").read()
@@ -23,7 +19,6 @@ def execute_ssh(host,port,username,password,command):
       no_error(host,port,"Running....")
       pid=os.system(f"sshpass -p {password} ssh -o StrictHostKeyChecking=no {username}@{host} -p {port} \"{command}\"")
       no_error(host,port,"Disconnected")
-
 def check_ip(host,port,username,password):
    global command
    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -39,15 +34,14 @@ def check_ip(host,port,username,password):
       else:
           threading.Thread(target=execute_ssh, args=(host,port,username,password,command)).start()
    del s
-
 if (os.name == 'nt'):
 	print("Windows is not supported!\n Use WSL/Docker or Linux to run it")
 	exit()
 else:
     check=os.popen("which sshpass").read()
     if (check.replace("\n","") == ""):
-        os.system("sudo apt update; sudo apt install sshpass -y")
-
+        print("Missing sshpass\nPlease install it first!")
+        exit()
 del sys.argv[0]
 try:
     file1 = open('ip.txt', 'r')
@@ -56,7 +50,6 @@ except FileNotFoundError as e:
 	exit()
 Lines = file1.readlines()
 file1.close()
-
 if ("--test" in sys.argv):
 	pass
 else:
@@ -79,7 +72,6 @@ else:
 	        command=f"cd \$HOME; curl {check} -o run.sh 2> /dev/null; sudo bash ./run.sh"
     else:
         print(f"{bcolors.CYAN} - Use command: {bcolors.YELLOW}{command}{bcolors.ENDC}")
-
 for line in Lines:
    info=line.split(" ")
    if (info != ""):
