d51ea3d3cb0b1dc94ea083367e118ce75636c72d
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2022-08-23 05:33:53 +0000

    add color

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 #!/bin/python3
 import threading, socket, os
 
+class bcolors:
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    ENDC = '\033[0m'
+    
+def error(host,port,arg):
+	print(f"{bcolors.RED}[!]{bcolors.YELLOW} {host}{bcolors.GREEN}:{bcolors.CYAN}{port} {bcolors.ENDC}| {bcolors.RED}{arg}{bcolors.ENDC}")
+
+def no_error(host,port,arg):
+	print(f"{bcolors.YELLOW}{host}{bcolors.GREEN}:{bcolors.CYAN}{port} {bcolors.ENDC}| {bcolors.GREEN}{arg}{bcolors.ENDC}")
+
 def execute_ssh(host,port,username,password,command):
    password=password.replace("\n","")
    check=os.popen(f"sshpass -p {password} ssh -o StrictHostKeyChecking=no {username}@{host} -p {port} whoami 2> /dev/null").read()
    if (check.replace("\n","") == ""):
-   	print(f"[!] {host}:{port} | Username or Password ERROR")
+   	error(host,port,"Username or Password ERROR")
    else:
-      print(f"{host}:{port} | Running....")
+      no_error(host,port,"Running....")
       os.system(f"sshpass -p {password} ssh -o StrictHostKeyChecking=no {username}@{host} -p {port} \"{command}\"")
-      print(f"{host}:{port} | Disconnected")
+      no_error(host,port,"Disconnected")
 
 def check_ip(host,port,username,password):
    global command
@@ -17,10 +30,10 @@ def check_ip(host,port,username,password):
    try:
       s.connect((host, int(port)))
    except:
-      print(f"[!] {host}:{port} | Failed")
+      error(host,port,"Failed")
    else:
       s.close()
-      print(f"{host}:{port} | Online")
+      no_error(host,port,"Online")
       threading.Thread(target=execute_ssh, args=(host,port,username,password,command)).start()
    del s
 
@@ -44,7 +57,7 @@ command=str(input("Input command or file: "))
 if os.path.isfile(command):
 	check=os.popen(f"curl --upload-file {command} https://temp.sh 2> /dev/null").read()
 	if (check == ""):
-		print("[!] Network ERROR!")
+		print(f"{bcolors.RED}[!] Network ERROR!")
 		exit()
 	else:
 	    command=f"cd $HOME; curl {check} -o run.sh 2> /dev/null; sudo bash ./run.sh"
